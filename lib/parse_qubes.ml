1f668567121bc7fe587a675699e68a044010c69c
Mindy <meetup@yomimono.org>
2019-05-13 16:19:48 -0400

    excise unused stuff from tests; reformat for qubes-only

--- lib/parse_qubes.ml
+++ lib/parse_qubes.ml
@@ -3,24 +3,98 @@
 (* it's not overly specified; for instance:
   - it's not clear if dst4, dst6, dstname can be specified in the same rule
 *)
+(* MCP: this is now specified: they can't *)
 
 open Angstrom
-open Parse
+
+type action = | Accept | Drop
+
+let pp_action f = function | Accept -> Fmt.string f "accept" | Drop -> Fmt.string f "drop"
+
+type family = | Inet | Inet6
+
+let a_whitespace_unit : unit t =
+  skip (function | ' '| '\t' -> true
+                 | _ -> false)
+
+let a_whitespace = skip_many1 a_whitespace_unit
+
+let a_ign_whitespace = skip_many a_whitespace_unit
+
+let some t = t >>| fun applied -> Some applied
+
+let a_number =
+  take_while1 (function '0'..'9' -> true | _ -> false) >>= fun str ->
+  match int_of_string str with
+    | i -> return i
+    | exception _ -> fail (Fmt.strf "Invalid number: %S" str)
+
+let a_number_range min' max' =
+  a_number >>= function | n when n <= max' && min' <= n -> return n
+                        | n -> fail (Fmt.strf "Number out of range: %d" n)
+
+let a_mask_bits ~af = a_number_range 0 (match af with | Inet -> 32
+                                                      | Inet6 -> 128)
+
+let a_cidr : Ipaddr.Prefix.t t =
+  let expand_ipv4 prefix =
+    let provided_octets = List.length (String.split_on_char '.' prefix) in
+    let padding = String.init ((4 - provided_octets)*2)
+        (function | i when i mod 2 = 0 -> '.'
+                  | _ -> '0')
+    in prefix ^ padding
+  in
+  let a_and_mask ip =
+    let af = match ip with Ipaddr.V4 _ -> Inet
+                         | V6 _ -> Inet6 in
+    option None (a_ign_whitespace *> char '/' *> some (a_mask_bits ~af))
+    >>| begin function
+      | None -> begin match af with Inet -> 32 | Inet6 -> 128 end
+      | Some mask -> mask
+    end >>= fun mask ->
+    begin match (Ipaddr.to_string ip) ^ "/" ^ (string_of_int mask)
+                |> Ipaddr.Prefix.of_string
+      with
+      | Error _ -> fail "invalid CIDR"
+      | Ok cidr -> return cidr
+    end
+  in
+  ( ( ( take_while1 ( function | '0'..'9' | '.'-> true
+                               | _ -> false
+      ) >>= (fun octets -> peek_char >>=
+              begin function (* TODO hack to make ipv6 work: *)
+                | Some (':'|'a'..'f'|'A'..'F') -> fail "not ipv4"
+                | _ -> return octets end
+            ) >>| expand_ipv4 >>| Ipaddr.V4.of_string >>= function
+        | Ok x -> a_and_mask (Ipaddr.V4 x)
+        | Error _ -> fail "invalid short ipv4 CIDR"
+      )
+      ) <|> (
+      (take_while1 ( function | '0'..'9' | 'a'..'f' | 'A'..'F' | ':' -> true
+                              | _ -> false
+         ) (* TODO expand_ipv6 *)
+       >>| Ipaddr.V6.of_string >>= (function
+           | Error _ -> fail "invalid ipv6 CIDR"
+           | Ok x -> a_and_mask (Ipaddr.V6 x)
+         )
+      )
+    )
+  )
 
 let q_action =
-  (string "drop" *> return (Block None))
+  (string "drop" *> return Drop)
   <|>
-  (string "accept" *> return Pass)
+  (string "accept" *> return Accept)
 
-let a_dst4 : (pf_af * Ipaddr.V4.Prefix.t) t =
+let a_dst4 : (family * Ipaddr.V4.Prefix.t) t =
   a_cidr >>= function
-  | Ipaddr.Prefix.(V4 x) -> return (Inet, x)
-  | Ipaddr.Prefix.(V6 _) -> fail "dst4 contains IPv6 CIDR"
+  | V4 x -> return (Inet, x)
+  | V6 _ -> fail "dst4 contains IPv6 CIDR"
 
-let a_dst6 : (pf_af * Ipaddr.V6.Prefix.t) t =
+let a_dst6 : (family * Ipaddr.V6.Prefix.t) t =
   a_cidr >>= function
-  | Ipaddr.Prefix.(V6 x) -> return (Inet6, x)
-  | Ipaddr.Prefix.(V4 _) -> fail "dst6 contains IPv4 CIDR"
+  | V6 x -> return (Inet6, x)
+  | V4 _ -> fail "dst6 contains IPv4 CIDR"
 
 let a_proto =
   choice [ string "tcp" *> return `tcp ;
@@ -32,31 +106,56 @@ let a_specialtarget =
   choice [ string "dns" *> return `dns ;
          ]
 
-let a_dstports : Parse.pf_op list t = (* NB only valid with tcp|udp *)
+type range = Range_inclusive of (int * int)
+
+let pp_range f (Range_inclusive (a, b)) = Fmt.pf f "[%d - %d]" a b
+
+let a_dstports : range list t = (* NB only valid with tcp|udp *)
   (* should use a_binary_op *)
   a_number_range 0 0xFFFF >>= fun low ->
   char '-' *>
   (* only accept ports that are >= 'low' and < 65336: *)
   a_number_range low 0xFFFF >>| fun high ->
-  [ Parse.Binary (Range_inclusive (low, high)) ]
+  [ (Range_inclusive (low, high)) ]
 
 let a_icmptype = a_number_range 0 1000 (* TODO look up max *)
 
 let a_dpi = string "NO" (* TODO this is not very well specified *)
 
-type qubes_rule =
-  { rule : Parse.pf_rule ;
-    source_ip : Ipaddr.t ;
-    number : int ;
+type proto = [ `udp | `tcp | `icmp ]
+let pp_proto f = function
+  | `udp -> Fmt.string f "udp"
+  | `tcp -> Fmt.string f "tcp"
+  | `icmp -> Fmt.string f "icmp"
+
+type rule =
+  { 
+    action : action;
+    proto : proto option;
+    specialtarget : [ `dns ] option;
+    dst : [ `any
+          | `hosts of Ipaddr.Prefix.t ]; (* TODO: ipv6, dsthosts *)
+    dstports : range list;
+    icmp_type : int option;
+    number : int; (* do we need this? *)
   }
 
-let pp_rule fmt {rule; source_ip; number} =
-  Fmt.pf fmt "@[<v>SOURCE_IP=%a %04d@,    %a@]"
-    Ipaddr.pp source_ip
-    number
-    Parse.pp_pf_rule rule
+let pp_specialtarget f _ = Fmt.string f "dns"
+let pp_dst f = function
+  | `any -> Fmt.string f "any"
+  | `hosts prefix -> Ipaddr.Prefix.pp f prefix
 
-let a_qubes_v4 ~source_ip ~number =
+let pp_rule fmt {action; proto; specialtarget; dst; dstports; icmp_type; number} =
+  Fmt.pf fmt "@[<v>%d %a %a %a %a %a %a@]"
+    number
+    (Fmt.option pp_proto) proto
+    (Fmt.option pp_specialtarget) specialtarget
+    pp_dst dst
+    (Fmt.list pp_range) dstports
+    Fmt.(option int) icmp_type
+    pp_action action
+
+let a_qubes_v4 ~source_ip:_ ~number =
   string "action=" *> q_action >>= fun action ->
   option (None, `any)
     ( (a_ign_whitespace *> string "dstname=" *>
@@ -66,10 +165,8 @@ let a_qubes_v4 ~source_ip ~number =
           (string "dst4=" *> a_dst4 >>| fun (af,x) -> af, Ipaddr.V4 x) ;
           (string "dst6=" *> a_dst6 >>| fun (af,x) -> af, Ipaddr.V6 x) ;
         ] >>| fun (af,cidr) ->
-       (Some af), `hosts [ Host_addr { negated = false ;
-                            if_or_cidr = CIDR cidr ;
-                          }] )
-    ) >>= fun (af, dst) ->
+       (Some af), `hosts cidr)
+    ) >>= fun (_af, dst) ->
   (* TODO note that it's not specified if multiple of these can be there*)
   option None (a_whitespace *> string "proto=" *> some a_proto) >>= fun proto ->
   option None (a_whitespace *> string "specialtarget=" *>
@@ -85,37 +182,16 @@ let a_qubes_v4 ~source_ip ~number =
       option None (a_whitespace *> string "icmptype=" *> some a_icmptype)
     | None | Some (`tcp | `udp) -> return None
   end >>= fun icmptype ->
-  option None (a_whitespace *> string "dpi=" *> some a_dpi) >>= fun dpi ->
+  option None (a_whitespace *> string "dpi=" *> some a_dpi) >>= fun _dpi ->
   end_of_input >>| fun () ->
-  let rule =
     { action ;
-      direction = Both_directions ; (* TODO *)
-      logopts = None ;
-      quick = true ;
-      ifspec = None ; (* TODO presumably on a NIC attached to the VM *)
-      route = None ;
-      af ;
-      protospec = (begin match proto with
-          | None -> None
-          | Some `udp -> Some (Proto_list [Name (String "udp")])
-          | Some `tcp -> Some (Proto_list [Name (String "tcp")])
-          | Some `icmp -> Some (Proto_list [Name (String "icmp")])
-        end ) ;
-      hosts =
-        From_to { from_host =
-                    `hosts [
-                      Host_addr
-                        { negated = false ;
-                          if_or_cidr = CIDR (Ipaddr.Prefix.of_addr source_ip) ;
-                        } ] ;
-                  from_port = [] ;
-                  from_os = [] ;
-                  to_host = dst ;
-                  to_port = dstports;
-                } ;
-      filteropts = [] ;
-    } in
-  { rule ; source_ip ; number ; }
+      proto;
+      specialtarget;
+      dst;
+      dstports;
+      icmp_type = icmptype;
+      number;
+    }
 
-let parse_qubes_v4 ~source_ip ~number entry : (qubes_rule, string) result =
+let parse_qubes_v4 ~source_ip ~number entry : (rule, string) result =
   parse_string (a_qubes_v4 ~source_ip ~number) entry
--- test/alcotest_parse.ml
+++ test/alcotest_parse.ml
@@ -1,51 +1,20 @@
 open Rresult
 
-
 let () =
   Printexc.record_backtrace true ;
   Logs.set_reporter @@ Logs_fmt.reporter ~dst:Format.std_formatter () ;
   Logs.(set_level @@ Some Debug)
 
-open Pf.Parse
+open Pf_qubes.Parse_qubes
 
-let alc_rule = Alcotest.testable pp_pf_rule (fun a b -> a = b)
-(*into_lines*)
-
-let alc_address = Alcotest.testable pp_pf_address (=)
-
-let alc_qubes = Alcotest.testable Pf.Parse_qubes.pp_rule (fun a b -> a = b)
+let alc_qubes = Alcotest.testable Pf_qubes.Parse_qubes.pp_rule (fun a b -> a = b)
 
 let parse_full a s = Angstrom.(parse_string (a <* end_of_input) s)
 
-let host_addr str = (* helper function *)
-  Host_addr { negated = false ;
-              if_or_cidr =
-                CIDR (Ipaddr.Prefix.of_string str |> function Some x -> x)
-            }
-
-let test_address_ipv6 () =
-  Alcotest.(check @@ result alc_address reject) "IS-OK fe80::123"
-    (Ok (IP (match Ipaddr.V6.of_string "fe80::123" with
-         | Some ip -> V6 ip | None -> failwith "Ipaddr.IPv6")))
-    (parse_full Pf.Parse.a_address "fe80::123") ;
-
-  Alcotest.(check @@ result alc_address reject) "IS-OK fe80::123%vlans0.123"
-    (Ok (IP (match Ipaddr.V6.of_string "fe80::123%vlans0.123" with
-         | Some ip -> V6 ip
-         | None -> Alcotest.fail "Ipaddr.IPv6 not supporting zone ID")))
-    (parse_full Pf.Parse.a_address "fe80::123%vlans0.123") ;
-
-  (* reject ifspec/vlanspec on global / NON-site-local addr: *)
-  Alcotest.(check @@ result reject pass)
-    "REJECT ifspec/vlanspec on global scope IPv6 addr: 1234::123%vlans0.123"
-    (Error "TODOwhattoputhere")
-    (parse_full Pf.Parse.a_address "1234::123%vlans0.123")
-
-open Pf.Parse_qubes
 
 let qubes str =
-  Pf.Parse_qubes.parse_qubes_v4
-    ~source_ip:Ipaddr.(of_string "127.0.0.1" |> function Some x -> x)
+  parse_qubes_v4
+    ~source_ip:Ipaddr.(of_string "127.0.0.1" |> function Ok x -> x | Error _ -> failwith "parse failure")
     ~number:0 str
 
 let test_qubes_empty () =
@@ -54,38 +23,26 @@ let test_qubes_empty () =
 
 let test_qubes_only_action () =
   Alcotest.(check @@ result alc_qubes string) "action=accept"
-    (Ok { Pf.Parse_qubes.source_ip = Ipaddr.(V4 V4.localhost) ;
-          number = 0 ;
-          rule = { Pf.Parse.empty_pf_rule with
-                   action = Pass ;
-                   quick = true ;
-                   hosts = From_to
-                       { from_host = `hosts [host_addr "127.0.0.1/32" ] ;
-                         from_port = [] ; from_os = [] ; to_host = `any ;
-                         to_port = [] ;
-                       } ;
-                 }
+    (Ok { number = 0 ;
+          action = Accept;
+          proto = None;
+          specialtarget = None;
+          dst = `any;
+          dstports = [];
+          icmp_type = None;
         }
     )
     (qubes "action=accept")
 
 let test_qubes_dns () =
   Alcotest.(check @@ result alc_qubes string) "action=accept dst4=8.8.8.8 proto=udp dstports=53-53"
-    (Ok { Pf.Parse_qubes.source_ip = Ipaddr.(V4 V4.localhost) ;
-          number = 0 ;
-          rule = { Pf.Parse.empty_pf_rule with
-                   action = Pass ;
-                   quick = true ;
-                   af = Some Inet ;
-                   protospec = Some (Proto_list [Name (String "udp")]) ;
-                   hosts = From_to
-                       { from_host = `hosts
-                             [host_addr "127.0.0.1/32" ] ;
-                         from_port = [] ; from_os = [] ;
-                         to_host = `hosts [host_addr "8.8.8.8/32"] ;
-                         to_port = [ Binary (Range_inclusive (53,53))] ;
-                       } ;
-                 }
+    (Ok { number = 0 ;
+          action = Accept;
+          proto = Some `udp;
+          specialtarget = None;
+          dst = `hosts (Ipaddr.Prefix.of_string_exn "8.8.8.8/32");
+          dstports = [Range_inclusive (53, 53)];
+          icmp_type = None;
         }
     )
     (qubes "action=accept dst4=8.8.8.8 proto=udp dstports=53-53")
@@ -93,21 +50,14 @@ let test_qubes_dns () =
 let test_qubes_ipv6 () =
   Alcotest.(check @@ result alc_qubes string)
     "action=drop dst6=2a00:1450:4000::/37 proto=tcp"
-    (Ok { Pf.Parse_qubes.source_ip = Ipaddr.(V4 V4.localhost) ;
+    (Ok { 
           number = 0 ;
-          rule = { Pf.Parse.empty_pf_rule with
-                   action = Block None ;
-                   quick = true ;
-                   af = Some Inet6 ;
-                   protospec = Some (Proto_list [Name (String "tcp")]) ;
-                   hosts = From_to
-                       { from_host = `hosts
-                             [host_addr "127.0.0.1/32" ] ;
-                         from_port = [] ; from_os = [] ;
-                         to_host = `hosts [host_addr "2a00:1450:4000::/37"] ;
-                         to_port = [] ;
-                       } ;
-                 }
+          action = Drop;
+          proto = Some `tcp;
+          specialtarget = None;
+          dst = `hosts (Ipaddr.Prefix.of_string_exn "2a00:1450:4000::/37" );
+          dstports = [];
+          icmp_type = None;
         }
     )
     (qubes "action=drop dst6=2a00:1450:4000::/37 proto=tcp")
@@ -115,36 +65,28 @@ let test_qubes_ipv6 () =
 let test_qubes_unimplemented () =
   Alcotest.(check @@ result alc_qubes string)
     "action=accept specialtarget=dns"
-    (Ok { Pf.Parse_qubes.source_ip = Ipaddr.(V4 V4.localhost) ;
+    (Ok { 
           number = 0 ;
-          rule = { Pf.Parse.empty_pf_rule with
-                   action = Pass ;
-                   quick = true ;
-                   hosts = From_to
-                       { from_host = `hosts
-                             [host_addr "127.0.0.1/32" ] ;
-                         from_port = [] ; from_os = [] ;
-                         to_host = `any; to_port = [] ;
-                       } ;
-                 }
+          action = Accept ;
+          proto = None;
+          specialtarget = Some `dns;
+          dst = `any;
+          dstports = [];
+          icmp_type = None;
         }
     )
     (qubes "action=accept specialtarget=dns") ;
+
   Alcotest.(check @@ result alc_qubes string)
     "action=drop proto=tcp specialtarget=dns"
-    (Ok { Pf.Parse_qubes.source_ip = Ipaddr.(V4 V4.localhost) ;
+    (Ok { 
           number = 0 ;
-          rule = { Pf.Parse.empty_pf_rule with
-                   action = Block None ;
-                   quick = true ;
-                   protospec = Some (Proto_list [Name (String "tcp")]) ;
-                   hosts = From_to
-                       { from_host = `hosts
-                             [host_addr "127.0.0.1/32" ] ;
-                         from_port = [] ; from_os = [] ;
-                         to_host = `any; to_port = [] ;
-                       } ;
-                 }
+          action = Drop;
+          proto = Some `tcp;
+          specialtarget = Some `dns;
+          dst = `any;
+          dstports = [];
+          icmp_type = None;
         }
     )
     (qubes "action=drop proto=tcp specialtarget=dns")
@@ -157,9 +99,6 @@ let tests =
       test_qubes_dns ;
       "Handling of IPv6 rules", `Quick, test_qubes_ipv6;
       "action=accept specialtarget=dns", `Quick, test_qubes_unimplemented
-    ] ;
-    "Primitives",
-    [ "address: IPv6", `Quick, test_address_ipv6 ;
     ] ;
   ]
 
